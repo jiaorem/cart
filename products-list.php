6c49b1140db9794b71da352f019231c038285338
Micoh Yabut <yabut.micohjomarie@auf.edu.ph>
2022-03-23 22:30:27 +0800

    Customized the design, added my own logo, updated the layout of products, and shopping cart

--- products-list.php
+++ products-list.php
@@ -7,19 +7,81 @@ $products_data = FileUtility::openCSV('products.csv');
 
 $products = Product::convertArrayToProducts($products_data);
 
-$customer = new Customer('John Doe', 'john@mail.com');
+$customer = new Customer('Micoh Yabut', 'yabut.micohjomarie@auf.edu.ph');
 ?>
+
 <html>
 <head>
-    <title>My Cart</title>
+    <title>Products</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
+    <link href="carousel.css" rel="stylesheet">
 </head>
 <body>
+<header >
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <img src="candy.png" href="products-list.php" height="50" width="50"><img>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-it em">
+                <a class="nav-link active headercon" aria-current="page" href="products-list.php">HOME</a>
+            </li>
+            <li class="nav-it em">
+                <a class="nav-link active headercon" aria-current="page" href="products-list.php">KPOP ALBUMS</a>
+            </li>
+            <li class="nav-it em">
+                <a class="nav-link active" aria-current="page" href="products-list.php">OFFICIAL LIGHTSTICKS</a>
+            </li>
+            <li class="nav-it em">
+                <a class="nav-link active" aria-current="page" href="products-list.php">OFFICIAL GOODS</a>
+            </li>
+        </ul>
+        <a href="shopping-cart.php">
+          <img src="cartlight.png" left="2000" position="fixed" width="50" height="50" >
+        </a>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
 
-<h1>Welcome <?php echo $customer->getName() ?>!</h1>
-<h2>Products</h2>
-<h4>
-    <a href="shopping-cart.php">Shopping Cart</a>
-</h4>
+</br></br></br></br>
+<h1 class="greet">Welcome <?php echo $customer->getName() ?>!</h1>
+<div class="container marketing">
+    <div class="row">
+      <div class="col-lg-4">
+        <img src="https://upload.wikimedia.org/wikipedia/en/8/8e/Twice_-_Formula_of_Love.png" width="140" height="140" focusable="false" width="100%" height="100%"></img>
+        <h2>Formula of Love</h2>
+        <p>The letters O and T represent the initials of Once—the group's official fandom's name—and Twice, respectively, while the heart symbol represents love; thus, the "formula of love" can be verbalized as "Once plus Twice equals love".</p>
+        <p><a class="btn btn-light" href="#">View details &raquo;</a></p>
+      </div>
+      <div class="col-lg-4">
+      <img src="https://upload.wikimedia.org/wikipedia/en/9/90/Twice_-_Eyes_Wide_Open.png" width="140" height="140" focusable="false" width="100%" height="100%"></img>
+
+        <h2>Eyes Wide Open</h2>
+        <p>“To express it in one phrase, I'd say, 'Opening your eyes to your senses. ' If we were able to learn about new senses during 'MORE & MORE,' this album explains that moment you truly register the senses we recognized in our last release.”</p>
+        <p><a class="btn btn-light" href="#">View details &raquo;</a></p>
+      </div>
+      <div class="col-lg-4">
+      <img src="https://i.scdn.co/image/ab67616d0000b27328e649c0e2d1a3a98ccf8ea3" width="140" height="140" focusable="false" width="100%" height="100%"></img>
+        <h2>More & More</h2>
+        <p>“When you fall in love, you want to more about that person. It's an album that reflects that desire to know more and want more of the person you fall in love with.” Momo explained, “I think I can describe it as an album that says, 'I want more from you!</p>
+        <p><a class="btn btn-light" href="#">View details &raquo;</a></p>
+      </div>
+    </div>
+
+</br>
+<h2>Available products</h2>
 
 <?php foreach ($products as $product): ?>
 
@@ -29,9 +91,9 @@ $customer = new Customer('John Doe', 'john@mail.com');
     <img src="<?php echo $product->getImage(); ?>" height="100px" />
     <p>
         <?php echo $product->getDescription(); ?><br/>
-        <span style="color: blue">PHP <?php echo $product->getPrice(); ?></span>
+        <span style="color: black">PHP <?php echo $product->getPrice(); ?></span>
         Qty <input type="number" name="quantity" class="quantity" value="0" />
-        <button type="submit">
+        <button type="submit" class="btn btn-danger">
             ADD TO CART
         </button>
     </p>
@@ -41,3 +103,31 @@ $customer = new Customer('John Doe', 'john@mail.com');
 
 </body>
 </html>
+
+
+<style>
+.greet{
+    position: relative;
+    left: 40%;
+    color: #000;
+    font-family: Bebas;
+    font-weight: 600;
+}
+body{
+    background-color: #89CFF0; 
+    font-family: Bebas;
+}
+.bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+@media (min-width: 1000px) {
+.bd-placeholder-img-lg {
+    font-size: 3.5rem;
+}
+}
+</style>
--- shopping-cart.php
+++ shopping-cart.php
@@ -8,7 +8,7 @@ $products_data = FileUtility::openCSV('products.csv');
 
 $products = Product::convertArrayToProducts($products_data);
 
-$customer = new Customer('John Doe', 'john@mail.com');
+$customer = new Customer('Micoh Yabut', 'yabut.micohjomarie@auf.edu.ph');
 
 $shoppingCart = new ShoppingCart($customer);
 $shoppingCartItems = $shoppingCart->getAllItems();
@@ -16,13 +16,179 @@ $shoppingCartItems = $shoppingCart->getAllItems();
 <html>
 <head>
     <title>My Cart</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <img src="candy.png" href="products-list.php" height="50" width="50"><img>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-it em">
+                <a class="nav-link active headercon" aria-current="page" href="products-list.php">HOME</a>
+            </li>
+            <li class="nav-it em">
+                <a class="nav-link active headercon" aria-current="page" href="products-list.php">KPOP ALBUMS</a>
+            </li>
+            <li class="nav-it em">
+                <a class="nav-link active" aria-current="page" href="products-list.php">OFFICIAL LIGHTSTICKS</a>
+            </li>
+            <li class="nav-it em">
+                <a class="nav-link active" aria-current="page" href="products-list.php">OFFICIAL GOODS</a>
+            </li>
+        </ul>
+        <a href="shopping-cart.php">
+          <img src="cartlight.png" left="2000" position="fixed" width="50" height="50" >
+        </a>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
+<section class="h-100 h-custom" style="background-color: #89CFF0;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col">
+        <div class="card">
+          <div class="card-body p-4">
 
-<h1>Welcome <?php echo $customer->getName() ?>!</h1>
-<h2>Shopping Cart</h2>
-<h4>
-    <a href="products-list.php">Shop More Products</a>
+            <div class="row">
+
+              <div class="col-lg-7">
+                <h5 class="mb-3"><a href="products-list.php" class="text-body"><i
+                      class="fas fa-long-arrow-alt-left me-2"></i>Continue shopping</a></h5>
+                <hr>
+
+                <div class="d-flex justify-content-between align-items-center mb-4">
+                  <div>
+                    <p class="mb-1">Shopping cart</p>
+                    <p class="mb-0">You have 3 items in your cart</p>
+                  </div>
+                </div>
+
+                <div class="card mb-3">
+                  <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                      <div class="d-flex flex-row align-items-center">
+                        <div>
+                          <img
+                            src="https://cafe24img.poxo.com/withdrama/web/product/big/202110/7f439d3264858bcfe1f375845f1cf2c7.jpg"
+                            class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
+                        </div>
+                        <div class="ms-3">
+                          <h5>Formula of Love</h5>
+                        </div>
+                      </div>
+                      <div class="d-flex flex-row align-items-center">
+                        <div style="width: 50px;">
+                          <h5 class="fw-normal mb-0">1</h5>
+                        </div>
+                        <div style="width: 80px;">
+                          <h5 class="mb-0">PHP 750</h5>
+                        </div>
+                        <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="card mb-3">
+                  <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                      <div class="d-flex flex-row align-items-center">
+                        <div>
+                          <img
+                            src="https://m.media-amazon.com/images/I/71mmeN+c9vL._SL1254_.jpg"
+                            class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
+                        </div>
+                        <div class="ms-3">
+                          <h5>Fancy You</h5>
+                        </div>
+                      </div>
+                      <div class="d-flex flex-row align-items-center">
+                        <div style="width: 50px;">
+                          <h5 class="fw-normal mb-0">1</h5>
+                        </div>
+                        <div style="width: 80px;">
+                          <h5 class="mb-0">PHP 500</h5>
+                        </div>
+                        <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="card mb-3">
+                  <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                      <div class="d-flex flex-row align-items-center">
+                        <div>
+                          <img
+                            src="https://m.media-amazon.com/images/I/51xszzWqszL.jpg"
+                            class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
+                        </div>
+                        <div class="ms-3">
+                          <h5>Feel Special</h5>
+                        </div>
+                      </div>
+                      <div class="d-flex flex-row align-items-center">
+                        <div style="width: 50px;">
+                          <h5 class="fw-normal mb-0">2</h5>
+                        </div>
+                        <div style="width: 80px;">
+                          <h5 class="mb-0">PHP 1100</h5>
+                        </div>
+                        <a href="#!" style="color: #cecece;"><i class="fas fa-trash-alt"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                    <hr class="my-4">
+
+                    <div class="d-flex justify-content-between">
+                    <p class="mb-2">Subtotal</p>
+                    <p class="mb-2">PHP 2,350.00</p>
+                    </div>
+
+                    <div class="d-flex justify-content-between">
+                    <p class="mb-2">Shipping</p>
+                    <p class="mb-2">PHP 150.00</p>
+                    </div>
+
+                    <div class="d-flex justify-content-between mb-4">
+                    <p class="mb-2">Total</p>
+                    <p class="mb-2">PHP 2,500.00</p>
+                    </div>
+
+                    <button type="button" class="btn btn-info btn-block btn-lg">
+                    <div class="d-flex justify-content-between">
+                        <span>Checkout <i class="fas fa-long-arrow-alt-right ms-2"></i></span>
+                    </div>
+                    </button>
+
+              </div>
+              
+
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 </h4>
 
 <?php if (count($shoppingCartItems) > 0): ?>
@@ -60,3 +226,15 @@ $shoppingCartItems = $shoppingCart->getAllItems();
 
 </body>
 </html>
+
+<style>
+body{
+  background-color: #89CFF0; 
+  font-family: Bebas;
+}
+@media (min-width: 1025px) {
+.h-custom {
+height: 100vh !important;
+}
+}
+</style>
